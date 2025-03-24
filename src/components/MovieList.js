8ab27dafb8661abe287cf2e961f1f8617dc45265
Vika8814 lab1 <mvv081105@gmail.com>
2025-03-24 16:34:33 +0200

    додала сторінку з фільмами

---
+++ src/components/MovieList.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import MovieCard from './MovieCard';
+import './MovieList.css';
+
+const MovieList = ({ movies }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="movie-list-container">
+      <input
+        type="text"
+        placeholder="Search by title..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="search-input"
+      />
+      <div className="movie-list">
+        {filteredMovies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MovieList;
