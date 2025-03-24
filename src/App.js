482fbe20a7ad7914b2cafc4d030393424727920e
Vika8814 lab1 <mvv081105@gmail.com>
2025-03-24 16:26:38 +0200

    додала App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Booking from './pages/Booking';
+
 function App() {
-  return <div>Hello, Cinema Booking!</div>;
+  return (
+    <div className="App">
+      <h1>Cinema Booking</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/booking/:id" element={<Booking />} />
+      </Routes>
+    </div>
+  );
 }
+
 export default App;
